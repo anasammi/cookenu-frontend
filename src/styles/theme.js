76b41bcd116c1234b9bdfb22125f20c90c5e3608
anasammi <a.sammi1103@gmail.com>
2023-10-31 20:22:06 -0300

    criada a pagina de login

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,19 +1,25 @@
 import { extendTheme } from "@chakra-ui/react";
-import chakraTheme from "@chakra-ui/theme";
 
 export const theme = extendTheme({
   components: {
     Button: {
       variants: {
         header: {
-            bg: "laranja.400"
+          bg: "laranja.400",
+        },
+        form: {
+          width: "100%",
+          bg: "laranja.400",
         },
       },
     },
   },
   colors: {
     laranja: {
-        400: "#FE7E02"
+      400: "#FE7E02",
+    },
+    cinza: {
+      200: "#d4d4d4"
     }
-  }
+  },
 });
